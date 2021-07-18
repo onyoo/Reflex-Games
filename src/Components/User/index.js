1399022fb5c5b36f195b9547880d971644a496fb
lo-k <kari.lonczak@gmail.com>
2021-07-18 15:26:53 -0700

    Display User's card count and handle if cards array is null

--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "../Card";
 
 export const User = ({name, cards, ...props }) => {
-  const userCards = cards;
-  const visibleCard = userCards[0]
+  const [userCards, setUserCards] = useState(cards);
+  const [visibleCard, setVisibleCard] = useState(null);
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      setVisibleCard(cards[0])
+    }
+  }, []);
 
   return (
     <div>
       {name}
+      <br/>
+      Card Count: {userCards.length}
+      <br/>
       {visibleCard}
     </div>
   );

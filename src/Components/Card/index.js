3b733e415b707cf1322c76adb81d3a9fe51f0a6d
onyoo <roberto9m9@gmail.com>
2021-07-18 13:20:13 -0700

    icon image to div

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -10,13 +10,15 @@ export const Card = ({ icons, ...props }) => {
       {icons.map((icon, idx) => {
         return (
           <Icon
-            image={icon.img}
+            image={"../../img/animal-icons-featured.png"} //{icon.img}
             names={icon.names}
             id={icon.id}
             style={{
               position: "absolute",
               top: `-${icon.position.top}px`,
-              left: `-${icon.position.right}px`,
+              left: `-${icon.position.left}px`,
+              right: `-${icon.position.right}px`,
+              bottom: `-${icon.position.bottom}px`,
               clip: `rect(${icon.position.top} ${icon.position.right} ${icon.position.bottom} ${icon.position.left})`,
             }}
           />
--- src/Components/Icon/index.js
+++ src/Components/Icon/index.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
 
 export const Icon = ({ image, names, style, id, ...props }) => {
+  console.log(style);
   return (
     <div
       id={id}
       style={{ position: "relative", display: "inline-block", padding: "1em" }}
     >
-      <img height="400px" src={image} style={{ ...style }} />
+      <div
+        style={{
+          backgroundImage: `url(${"./img/animal-icons-featured-pub.png"})`,
+          backgroundPosition: `right ${style.right} bottom ${style.top}`,
+          backgroundSize: "400px",
+          height: "60px",
+          width: "60px",
+          borderRadius: "3em",
+        }}
+      >
+        {id}
+      </div>
+      {/* <img height="400px" src={image} style={{ ...style }} /> */}
       {/* {names.join(", ")} */}
       <br />
       <br />
